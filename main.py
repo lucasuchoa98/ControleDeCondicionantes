38975037a33540063eebd486f4c4e83c1c194293
lucasuchoa98 <lucasuchoalg@gmail.com>
2022-04-17 20:57:29 -0300

    implementacao de db

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
 
 # Dependency
 def get_db():

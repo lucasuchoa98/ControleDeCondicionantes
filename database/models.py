bc569847c0cb09785fb723270ea18c1466386a00
lucasuchoa98 <lucasuchoalg@gmail.com>
2022-08-02 22:36:50 -0300

    models and servicer added fully

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import declarative_base, relationship
 
 
@@ -17,3 +17,17 @@ class Favorite(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     symbol = Column(String)
     user_id = Column(Integer, ForeignKey('user.id'))
+
+
+class Customer(Base):
+    __tablename__ = 'custumer'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    licence = relationship('Licence', backref='custumer', lazy='subquery')
+
+class Licence(Base):
+    __tablename__ = 'licence'
+    licence_number =Column(Integer, primary_key=True)
+    agency = Column(String)
+    published_date = Column(Date)
+    maturity = Column(Date)
--- services.py
+++ services.py
@@ -4,8 +4,40 @@ from sqlalchemy.ext.asyncio.session import async_session
 from sqlalchemy.future import select
 from sqlalchemy import delete
 
-from database.models import User, Favorite
+from database.models import Customer, Licence, User, Favorite
 from database.connection import async_session
+
+class CustomerService:
+    async def create_customer(name:str):
+        async with async_session() as session:
+            session.add(Customer(name=name))
+            await session.commit()
+
+    async def delete_customer(custumer_id: int):
+        async with async_session() as session:
+            await session.execute(delete(Customer).where(Customer.id==custumer_id))
+            await session.commit()
+
+    async def list_user():
+        async with async_session() as session:
+            result = await session.execute(select(Customer))
+            return result.scalars().all()
+    
+    async def get_by_id(customer_id):
+        async with async_session() as session:
+            result = await session.execute(select(Customer).where(Customer.id==Customer))
+            return result.scalar()
+
+class LicenceService:
+    async def add_licence(customer_id: int, licence_number: str, agency:str, published_date: date, maturity:date):
+        async with async_session() as session:
+            session.add(Licence(customer_id=customer_id, licence_number= licence_number, agency = agency, published_date = published_date, maturity= maturity))
+            await session.commit()
+
+    async def remove_licence(customer_id: int, licence_number: str):
+        async with async_session() as session:
+            await session.execute(delete(Licence).where(Licence.customer_id==customer_id, Licence.licence_number==licence_number))
+            await session.commit()
 
 
 class UserService:
